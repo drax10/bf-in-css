12d9d91389d84f4b83d0c1d8b83edc3560bb2af1
Joshua Draxten <JoshuaDraxten@live.com>
2018-11-04 21:07:26 -0500

    💁‍♀️ Purely Asthetic Facelift

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const initialMemory = ""
 const fillList = (size, fillWith) => [...Array(size)].map(()=>fillWith);
 
 var config = {
-  memory: Object.assign( fillList(20, 0), initialMemory.split('') ),
-  commands: Object.assign( fillList(20, ''), program.split('') )
+  memory: Object.assign( fillList(23*3, 0), initialMemory.split('') ),
+  commands: Object.assign( fillList(23*3, ''), program.split('') )
 };
 
 module.exports = config;
